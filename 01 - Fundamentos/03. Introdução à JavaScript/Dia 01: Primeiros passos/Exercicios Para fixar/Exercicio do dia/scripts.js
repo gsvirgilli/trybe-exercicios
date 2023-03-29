65c404acbed873b189c469593d9cb79938e3f0ee
Guilherme Virgilli <gsvirgilli@gmail.com>
2023-03-28 22:44:40 -0300

    Exercicio até o numero 11

--- 01 - Fundamentos/03. Introdução à JavaScript/Dia 01: Primeiros passos/Exercicios Para fixar/Exercicio do dia/scripts.js
+++ 01 - Fundamentos/03. Introdução à JavaScript/Dia 01: Primeiros passos/Exercicios Para fixar/Exercicio do dia/scripts.js
@@ -1,6 +1,3 @@
-let a =9;
-let b =3;
-
 /* 001 */
 
 /* 02 */
@@ -124,3 +121,48 @@ console.log(isEven);
 
 /* 10 */
 
+
+let costOfProduct = 1;
+let saleValue = 3;
+
+if (costOfProduct >= 0 && saleValue >= 0) {
+  let totalCostOfProduct = costOfProduct * 1.2;
+  let totalProfit = (saleValue - totalCostOfProduct) * 1000;
+  console.log(totalProfit);
+} else {
+  console.log("Error, os valores não podem ser negativos!");
+};
+
+/* 11 */
+
+
+let aliquotINSS;
+let aliquotIR;
+
+let grossSalary = 2000.00;
+
+if (grossSalary <= 1556.94) {
+  aliquotINSS = grossSalary * 0.08;
+} else if (grossSalary <= 2594.92) {
+  aliquotINSS = grossSalary * 0.09;
+} else if (grossSalary <= 5189.82) {
+  aliquotINSS = grossSalary * 0.11;
+} else {
+  aliquotINSS = 570.88;
+}
+
+let baseSalary = grossSalary - aliquotINSS;
+
+if (baseSalary <= 1903.98) {
+  aliquotIR = 0;
+} else if (baseSalary <= 2826.65) {
+  aliquotIR = (baseSalary * 0.075) - 142.80;
+} else if (baseSalary <= 3751.05) {
+  aliquotIR = (baseSalary * 0.15) - 354.80;
+} else if (baseSalary <= 4664.68) {
+  aliquotIR = (baseSalary * 0.225) - 636.13;
+} else {
+  aliquotIR = (baseSalary * 0.275) - 869.36;
+};
+
+console.log("Salário: " + (baseSalary - aliquotIR));
