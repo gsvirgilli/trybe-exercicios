151ebc9b84aa657e9d98fcff4458d0aa2d29d67f
Guilherme Virgilli <gsvirgilli@gmail.com>
2023-04-03 16:14:26 -0300

    03 PARA FIXAR

---
+++ 01 - Fundamentos/03. Introdução à JavaScript/Dia 04: Objetos/Exercicios Para fixar/03 - Para fixar/scripts.js
@@ -0,0 +1,11 @@
+let student = {}
+
+function addKey(object,key,valor) {
+    object[key] = valor;
+}
+
+addKey(student, 'nome', 'Guilherme Silva Virgilli');
+addKey(student, 'email', 'gsvirgilli@gmail.com');
+addKey(student, 'telefone', '62 98276-4571');
+addKey(student, 'gitHub', 'github.com/gsvirgilli');
+addKey(student, 'linkdin', 'linkdin.com/gsvirgilli');
