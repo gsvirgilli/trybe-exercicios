eb38520ed969bcbd7e2aa6d9abeca51ed7871d3a
Guilherme Silva Virgilli <gsvirgilli@gmail.com>
2023-05-05 12:38:11 -0300

    Para Fixar

---
+++ 01 - Fundamentos/07. Highter Order Functions do JavaScrip ES6/Dia 01: Introdução a Higher Order Functions - sort e map/Exercicio Para Fixar/2 - Para Fixar /script.js
@@ -0,0 +1,18 @@
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+numbers.find((num) =>num**3 === 0);
+
+console.log(numbers.find((num) =>num % 3 === 0 && num % 5 === 0));
+
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+console.log(names.find((name) => name.length === 5));
+
+const musicas = [
+    { id: '31031685', title: 'Partita in C moll BWV 997' },
+    { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+    { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+];
+
+console.log(musicas.find((musica) =>musica.id === '31031685'))
