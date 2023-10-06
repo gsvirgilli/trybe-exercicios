87966d8da514aa63210cfd7d571a9b7ccbfb1e2b
Guilherme Virgilli <gsvirgilli@gmail.com>
2023-10-06 02:19:51 -0300

    Desafio 09

---
+++ 03. Back-end/04. Introdução ao desenvolvimento Web com Node.js/Dia 01: Node.js: Runtime Assíncrono/Exercicios/01-Exercicio/bmi.js.js
@@ -0,0 +1,29 @@
+const calcular = 1;
+
+const ola = () => {
+    console.log("Olá mundo!");
+}
+
+const calcularIMC = (peso, altura) => {
+    return peso / (altura * altura);
+}
+
+const calcularIMC2 = (peso, altura) => peso / (altura * altura);
+
+const calcularIMC3 = (peso, altura) => {
+    return peso / (altura * altura);
+}
+
+const calcularIMC4 = (peso, altura) => {
+    return peso / (altura * altura);
+}   
+
+const calcularIMC5 = (peso, altura) => {
+    return peso / (altura * altura);
+}
+
+const calcularIMC6 = (peso, altura) => {
+    return peso / (altura * altura);
+}
+
+let 
