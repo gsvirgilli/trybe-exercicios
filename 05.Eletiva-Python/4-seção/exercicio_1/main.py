d8d40765255c93d6afa2fec73aed841084fa5b52
Guilherme Virgilli <gsvirgilli@gmail.com>
2024-08-23 17:05:01 -0300

    Mais uma mudança

--- 05.Eletiva-Python/4-seção/exercicio_1/main.py
+++ 05.Eletiva-Python/4-seção/exercicio_1/main.py
@@ -3,6 +3,24 @@ from fastapi import FastAPI
 app = FastAPI(title="Olá, mundo!")
 
 
+def my_dependency(query_string: str = None):
+    ...
+    return query_string
+
+
+@app.get("/")
+async def my_route(dependency: str = Depends(my_dependency)):
+    return {"message": dependency}
+
+
 @app.get("/")
 def home():
     return {"message": "Hello World"}
+
+
+@app.middleware("http")
+async def my_custom_middleware(request: Request, call_next):
+    ...
+    response = await call_next(request)
+    ...
+    return response
