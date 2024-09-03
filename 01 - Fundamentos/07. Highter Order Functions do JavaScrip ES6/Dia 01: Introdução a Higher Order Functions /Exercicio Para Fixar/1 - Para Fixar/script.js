040adfe7d6848d4c5c279d3452ce3030aea78518
Guilherme Virgilli <gsvirgilli@gmail.com>
2024-09-03 18:09:17 -0300

    asd

---
+++ 01 - Fundamentos/07. Highter Order Functions do JavaScrip ES6/Dia 01: Introdução a Higher Order Functions /Exercicio Para Fixar/1 - Para Fixar/script.js
@@ -0,0 +1,8 @@
+const emailListInData = [
+    'roberta@email.com',
+    'paulo@email.com',
+    'anaroberta@email.com',
+    'fabiano@email.com',
+  ];
+  
+emailListInData.forEach((item)=>console.log(`O email ${item} está cadastrado em nosso banco de dados!`));
--- 05.Eletiva-Python/4-seção/exercicio_1/main.py
+++ 05.Eletiva-Python/4-seção/exercicio_1/main.py
@@ -1,14 +1,1 @@
-from pydantic import BaseModel
-from fastapi import FastAPI
 
-app = FastAPI()
-
-
-class User(BaseModel):
-    name: str
-    age: int
-
-
-@app.post("/users")
-def create_user(user: User):
-    return user
